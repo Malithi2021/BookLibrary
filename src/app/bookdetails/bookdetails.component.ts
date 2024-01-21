678ffbaf25098d8b7bdd4a1a50dc0e064bdbd0db
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2024-01-21 18:22:03 +0530

    form add

--- src/app/bookdetails/bookdetails.component.ts
+++ src/app/bookdetails/bookdetails.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { BookingService } from '../booking.service';
 import { BookItem } from '../book-item';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bookdetails',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   template: `
   <article>
     <img class="listing-photo" [src]="BookItem?.photo"
@@ -17,11 +21,25 @@ import { BookItem } from '../book-item';
       <p class="listing-location">{{BookItem?.author}}, {{BookItem?.price}}</p>
     </section>
     <section class="listing-features">
-      <h2 class="section-heading">About this Book</h2>
+      <h2 class="section-heading">About this book </h2>
       <ul>
         <li>Units available: {{BookItem?.availableUnits}}</li>
-        <li>Is this book available online: {{BookItem?.online}}</li>
+        <li>Is this available online: {{BookItem?.online}}</li>
       </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Order now to reserve your book</h2>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <label for="first-name">First Name</label>
+        <input id="first-name" type="text" formControlName="firstName">
+
+        <label for="last-name">Last Name</label>
+        <input id="last-name" type="text" formControlName="lastName">
+
+        <label for="email">Email</label>
+        <input id="email" type="email" formControlName="email">
+        <button type="submit" class="primary">Apply now</button>
+      </form>
     </section>
   </article>
 `,
@@ -32,8 +50,22 @@ export class BookdetailsComponent {
   BookingService = inject(BookingService);
   BookItem: BookItem | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const bookId = Number(this.route.snapshot.params['id']);
     this.BookItem = this.BookingService.getBookItemById(bookId);
+  }
+
+  submitApplication() {
+    this.BookingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
